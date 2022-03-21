ac61b0f7c94b213ec0c9f14ce320c7af718850f6
Ganesh <ganeshkanwade7745@gmail.com>
2022-03-21 20:18:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar tilte="text" abouttext="About us" contacttext="contact us" heading="ganesh" />
+    </>
+  );
+}
+
+export default App;
